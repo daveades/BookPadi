551c8337056de8a41028cae3de1a4739c8e10191
David Adesanya <82074718+daveades@users.noreply.github.com>
2025-01-26 19:12:44 +0000

    Refactor application structure: initialize database and register blueprints in create_app function

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from .utils.db import init_db
+from .routes.user_routes import user_bp
 
 def create_app():
     app = Flask(__name__)
 
+    # Initialize the database connection
+    init_db()
+
+    # Register blueprints
+    app.register_blueprint(user_bp, url_prefix='/api')
+
+    @app.route('/')
+    def home():
+        return 'Welcome to BookPadi!'
+    
     return app
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,6 @@
 from . import create_app
-from .utils.db import init_db
-from .routes.user_routes import user_bp
 
 app = create_app()
-
-#initialize the database connection
-init_db()
-
-#Register blueprints
-app.register_blueprint(user_bp, url_prefix='/api')
-
-@app.route('/')
-def home():
-    return 'Welcome to BookPadi!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
