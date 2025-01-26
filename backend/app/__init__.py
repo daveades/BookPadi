568f6b2d7a9afdee880e74ace0ee036db3f12ad6
David Adesanya <82074718+daveades@users.noreply.github.com>
2025-01-26 20:06:57 +0000

    Add JWT secret key configuration to the application

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from .utils.db import init_db
 from .routes.user_routes import user_bp
+from app.config import JWT_SECRET_KEY
 
 def create_app():
     app = Flask(__name__)
+
+    #JWT Config
+    app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
 
     # Initialize the database connection
     init_db()
