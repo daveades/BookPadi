fc8668c0fed6c530f6a7d87a93cb806fde90aaf9
DAVE <jesugbohunmi23@gmail.com>
2025-09-12 11:24:53 +0100

    Comment out the Categories link in the Header component

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -35,7 +35,7 @@ const Header: React.FC = () => {
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/books">Books</Link></li>
-                        <li><Link to="/categories">Categories</Link></li>
+                        {/* <li><Link to="/categories">Categories</Link></li> */}
                         <li><Link to="/about">About</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
                     </ul>
