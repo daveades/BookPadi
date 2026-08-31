f1fa37225eea1a77f6feb455228075c022b57f30
dave <jesugbohunmi23@gmail.com>
2026-08-31 20:34:15 +0100

    Delet old project files in preparation for new phase

--- backend/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MONGODB_URI = os.getenv("MONGODB_URI")
-MONGODB_USERNAME = os.getenv("MONGODB_USERNAME")
-MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
